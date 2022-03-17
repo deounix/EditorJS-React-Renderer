deea3d059ec01d9c6e068354561ac8add0655f67
Mohammad Almoghrabi <deounix@gmail.com>
2022-03-17 09:42:13 +0200

    add link & raw & code renderer as optional

--- index.jsx
+++ index.jsx
@@ -1,99 +1,233 @@
 /** EditorJS-React Renderer
-  *
-  * A small library that provides functions to parse and render data saved by
-  * EditorJS into react components
-  *
-  * @version 1.0.0
-  * @created - 2019.08.20
-  * @author - Adombang Munang Mbomndih (Bomdi) <dzedock@gmail.com> (https://bomdisoft.com)
-  *
-  * Version History
-  * ---------------
-  * @version 1.0.1 - 2020.02.12 - Covert functions to React component
-  *                             - Add CodeBoxOutput
-  *                             - Adombang Munang Mbomndih
-  * @version 1.0.2 - 2020.05.21 - Add key to list items - Adombang Munang Mbomndih
-  * @version 1.0.3 - 2020.07.17 - Add config parameter - Adombang Munang Mbomndih
-  * @version 1.1.0 - 2021.04.11 - Add classNames parameter - Adombang Munang Mbomndih
-  *
-  */
+ *
+ * A small library that provides functions to parse and render data saved by
+ * EditorJS into react components
+ *
+ * @version 1.0.0
+ * @created - 2019.08.20
+ * @author - Adombang Munang Mbomndih (Bomdi) <dzedock@gmail.com> (https://bomdisoft.com)
+ *
+ * Version History
+ * ---------------
+ * @version 1.0.1 - 2020.02.12 - Covert functions to React component
+ *                             - Add CodeBoxOutput
+ *                             - Adombang Munang Mbomndih
+ * @version 1.0.2 - 2020.05.21 - Add key to list items - Adombang Munang Mbomndih
+ * @version 1.0.3 - 2020.07.17 - Add config parameter - Adombang Munang Mbomndih
+ * @version 1.1.0 - 2021.04.11 - Add classNames parameter - Adombang Munang Mbomndih
+ *
+ */
 
 //#region imports
-import React from 'react';
-import HeaderOutput from './renderers/header/index.jsx';
-import ParagraphOutput from './renderers/paragraph/index.jsx';
-import ImageOutput from './renderers/image/index.jsx';
-import VideoOutput from './renderers/video/index.jsx';
-import EmbedOutput from './renderers/embed/index.jsx';
-import ListOutput from './renderers/list/index.jsx';
-import QuoteOutput from './renderers/quote/index.jsx';
-import ChecklistOutput from './renderers/checklist/index.jsx';
-import WarningOutput from './renderers/warning/index.jsx';
-import TableOutput from './renderers/table/index.jsx';
-import DelimiterOutput from './renderers/delimiter/index.jsx';
-import CodeBoxOutput from './renderers/codeBox/index.jsx';
+import React from "react";
+import HeaderOutput from "./renderers/header/index.jsx";
+import ParagraphOutput from "./renderers/paragraph/index.jsx";
+import ImageOutput from "./renderers/image/index.jsx";
+import VideoOutput from "./renderers/video/index.jsx";
+import EmbedOutput from "./renderers/embed/index.jsx";
+import ListOutput from "./renderers/list/index.jsx";
+import QuoteOutput from "./renderers/quote/index.jsx";
+import ChecklistOutput from "./renderers/checklist/index.jsx";
+import WarningOutput from "./renderers/warning/index.jsx";
+import TableOutput from "./renderers/table/index.jsx";
+import DelimiterOutput from "./renderers/delimiter/index.jsx";
+import CodeBoxOutput from "./renderers/codeBox/index.jsx";
 //#endregion
 
 const Output = ({ data, style, classNames, config, renderers }) => {
-  if (!data || typeof data !== 'object') return '';
-  if (!style || typeof style !== 'object') style = {};
-  if (!classNames || typeof classNames !== 'object') classNames = {};
-  if (!config || typeof config !== 'object') config = {};
-  if (!renderers || typeof renderers !== 'object') renderers = {};
+  if (!data || typeof data !== "object") return "";
+  if (!style || typeof style !== "object") style = {};
+  if (!classNames || typeof classNames !== "object") classNames = {};
+  if (!config || typeof config !== "object") config = {};
+  if (!renderers || typeof renderers !== "object") renderers = {};
 
   return data.blocks.map((block, i) => {
     let Renderer = null;
 
     switch (block.type.toLowerCase()) {
-      case 'code':
-        Renderer = renderers.code || CodeBoxOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.code || {}} config={ config.code || {}} classNames={ classNames.code || {}} />;
-      case 'codebox':
+      case "link":
+        Renderer = renderers.link || <React.Fragment />;
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.link || {}}
+            config={config.link || {}}
+            classNames={classNames.link || {}}
+          />
+        );
+      case "raw":
+        Renderer = renderers.raw || <React.Fragment />;
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.raw || {}}
+            config={config.raw || {}}
+            classNames={classNames.raw || {}}
+          />
+        );
+      case "code":
+        Renderer = renderers.code || <React.Fragment />;
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.code || {}}
+            config={config.code || {}}
+            classNames={classNames.code || {}}
+          />
+        );
+      case "codebox":
         Renderer = renderers.codeBox || CodeBoxOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.codeBox || {}} config={ config.codeBox || {}} classNames={ classNames.codeBox || {}} />;
-      case 'header':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.codeBox || {}}
+            config={config.codeBox || {}}
+            classNames={classNames.codeBox || {}}
+          />
+        );
+      case "header":
         Renderer = renderers.header || HeaderOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.header || {}} config={ config.header || {}} classNames={ classNames.header || {}} />;
-      case 'paragraph':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.header || {}}
+            config={config.header || {}}
+            classNames={classNames.header || {}}
+          />
+        );
+      case "paragraph":
         Renderer = renderers.paragraph || ParagraphOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.paragraph || {}} config={ config.paragraph || {}}
-          classNames={ classNames.paragraph || {}} />;
-      case 'image':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.paragraph || {}}
+            config={config.paragraph || {}}
+            classNames={classNames.paragraph || {}}
+          />
+        );
+      case "image":
         Renderer = renderers.image || ImageOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.image || {}} config={ config.image || {}} classNames={ classNames.image || {}} />;
-      case 'video':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.image || {}}
+            config={config.image || {}}
+            classNames={classNames.image || {}}
+          />
+        );
+      case "video":
         Renderer = renderers.video || VideoOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.video || {}} config={ config.video || {}} classNames={ classNames.video || {}} />;
-      case 'embed':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.video || {}}
+            config={config.video || {}}
+            classNames={classNames.video || {}}
+          />
+        );
+      case "embed":
         Renderer = renderers.embed || EmbedOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.embed || {}} config={ config.embed || {}} classNames={ classNames.embed || {}} />;
-      case 'table':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.embed || {}}
+            config={config.embed || {}}
+            classNames={classNames.embed || {}}
+          />
+        );
+      case "table":
         Renderer = renderers.table || TableOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.table || {}} config={ config.table || {}} classNames={ classNames.table || {}} />;
-      case 'list':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.table || {}}
+            config={config.table || {}}
+            classNames={classNames.table || {}}
+          />
+        );
+      case "list":
         Renderer = renderers.list || ListOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.list || {}} config={ config.list || {}} classNames={ classNames.list || {}} />;
-      case 'checklist':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.list || {}}
+            config={config.list || {}}
+            classNames={classNames.list || {}}
+          />
+        );
+      case "checklist":
         Renderer = renderers.checklist || ChecklistOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.checklist || {}} config={ config.checklist || {}}
-          classNames={ classNames.checklist || {}} />;
-      case 'quote':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.checklist || {}}
+            config={config.checklist || {}}
+            classNames={classNames.checklist || {}}
+          />
+        );
+      case "quote":
         Renderer = renderers.quote || QuoteOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.quote || {}} config={ config.quote || {}} classNames={ classNames.quote || {}} />;
-      case 'warning':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.quote || {}}
+            config={config.quote || {}}
+            classNames={classNames.quote || {}}
+          />
+        );
+      case "warning":
         Renderer = renderers.warning || WarningOutput;
-        return <Renderer key={ i } data={ block.data } style={ style.warning || {}} config={ config.warning || {}}
-          classNames={ classNames.warning || {}} />;
-      case 'delimiter':
+        return (
+          <Renderer
+            key={i}
+            data={block.data}
+            style={style.warning || {}}
+            config={config.warning || {}}
+            classNames={classNames.warning || {}}
+          />
+        );
+      case "delimiter":
         Renderer = renderers.delimiter || DelimiterOutput;
-        return <Renderer key={ i } style={ style.delimiter || {}} config={ config.delimiter || {}} classNames={ classNames.delimiter || {}} />;
+        return (
+          <Renderer
+            key={i}
+            style={style.delimiter || {}}
+            config={config.delimiter || {}}
+            classNames={classNames.delimiter || {}}
+          />
+        );
 
-      default: return '';
+      default:
+        return "";
     }
   });
 };
 
 export {
-  HeaderOutput, ParagraphOutput, ImageOutput, VideoOutput, EmbedOutput, TableOutput, CodeBoxOutput, ListOutput, QuoteOutput,
-  ChecklistOutput, WarningOutput, DelimiterOutput, Output as default
+  HeaderOutput,
+  ParagraphOutput,
+  ImageOutput,
+  VideoOutput,
+  EmbedOutput,
+  TableOutput,
+  CodeBoxOutput,
+  ListOutput,
+  QuoteOutput,
+  ChecklistOutput,
+  WarningOutput,
+  DelimiterOutput,
+  Output as default,
 };
